3e76069bdc942b48a69334c3b68c03a21bf637ca
S-Mann <sukhinder.mann@gmail.com>
2021-04-10 19:03:19 +0100

    ⚗️ added rust debugger and played around with rust-analyzer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use rand::Rng;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess a number");
+    println!("Please intput your number");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Rip");
+
+    println!("You guessed: {}", guess);
 }
